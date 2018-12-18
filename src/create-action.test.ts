5bcc6f5c5bf15c2e2edc1f39d1e31f2d0ff80feb
Kenneth Powers <ken@kenpowers.net>
2018-12-18 16:17:56 -0500

    feat(create-action): prevent duplicate action types in development

--- src/create-action.test.ts
+++ src/create-action.test.ts
@@ -2,50 +2,76 @@ import { bindActionCreators } from 'redux';
 import createAction from './create-action';
 
 test('creates actions with constant payload creator', () => {
-  const ac = createAction<string>('foo', () => 'bar');
+  const ac = createAction<string>('foo1', () => 'bar');
   expect(ac()).toEqual({
-    type: 'foo',
+    type: 'foo1',
     payload: 'bar',
   });
 });
 
 test('creates actions with untyped identity payload creator', () => {
-  const ac = createAction<string>('foo');
+  const ac = createAction<string>('foo2');
   expect(ac('bar')).toEqual({
-    type: 'foo',
+    type: 'foo2',
     payload: 'bar',
   });
 });
 
 test('creates actions with explicit payload creator', () => {
-  const ac = createAction<string, [number]>('foo', num => `${num}`);
+  const ac = createAction<string, [number]>('foo3', num => `${num}`);
   expect(ac(4)).toEqual({
-    type: 'foo',
+    type: 'foo3',
     payload: '4',
   });
 });
 
 test('creates actions with explicit payload creator (many args)', () => {
-  const ac = createAction<string, [number, boolean, string]>('foo', num => `${num * 2}`);
+  const ac = createAction<string, [number, boolean, string]>('foo4', num => `${num * 2}`);
   expect(ac(4, true, 'bar')).toEqual({
-    type: 'foo',
+    type: 'foo4',
     payload: '8',
   });
 });
 
 test('works with bindActionCreators from redux', () => {
-  const ac1 = createAction<string>('foo', () => 'baz');
-  const ac2 = createAction<string, [string]>('bar', s => s);
+  const ac1 = createAction<string>('foo5', () => 'baz');
+  const ac2 = createAction<string, [string]>('bar5', s => s);
   const acs = bindActionCreators({
     ac1,
     ac2,
   }, (action) => {
-    expect(action.type).toBe('foo');
+    expect(action.type).toBe('foo5');
     return action;
   });
   acs.ac1();
 });
 
 test('toString', () => {
-  expect(createAction('foo').toString()).toBe('foo');
+  expect(createAction('foo6').toString()).toBe('foo6');
+});
+
+const setNodeEnv = (env: string) => {
+  const old = process.env.NODE_ENV;
+  process.env.NODE_ENV = env;
+  return () => {
+    process.env.NODE_ENV = old;
+  };
+};
+
+test('does not allow colliding action types in development', () => {
+  // Development
+  expect(() => {
+    const reset = setNodeEnv('development');
+    createAction<string>('banana');
+    createAction<string>('banana');
+    reset();
+  }).toThrowError(/DUPLICATE/);
+
+  // Production
+  expect(() => {
+    const reset = setNodeEnv('production');
+    createAction<string>('banana');
+    createAction<string>('banana');
+    reset();
+  }).not.toThrowError(/DUPLICATE/);
 });
--- src/create-action.ts
+++ src/create-action.ts
@@ -12,11 +12,27 @@ export interface TsActionCreator<T, A extends any[] = [T?]> {
 export type PayloadCreator<P, A extends any[] = [P?]> = (...args: A) => P;
 const identity = <T extends any[]>(...arg: T): T[0] => arg[0];
 
+// As long as we're in development mode we will store already-created action
+// types for debug purposes.
+let debugTypeMap: { [key: string]: true };
+if (process.env.NODE_ENV !== 'production') {
+  debugTypeMap = {};
+}
+
 // eslint-disable-next-line arrow-parens
 export default <P, A extends any[] = [P?]>(
   type: string,
   pc: PayloadCreator<P, A> = identity,
 ): TsActionCreator<P, A> => {
+  // Make sure to avoid action type collisions. This code is removed in
+  // production.
+  if (process.env.NODE_ENV !== 'production') {
+    if (debugTypeMap && debugTypeMap[type]) {
+      throw new Error(`DUPLICATE ACTION CREATOR! type: ${type}`);
+    }
+    debugTypeMap[type] = true;
+  }
+  // Continue with creating an action creator
   const ac = (...args: any[]): TsAction<P> => ({
     type,
     payload: pc(...args as A),
